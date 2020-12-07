f5fdf970048289219ad15558ce8ca0f6af5ae0fe
emilyjmaloney <emilyjean.maloney@gmail.com>
2020-12-07 12:28:55 +0000

    Completed 1-7 of 10

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,4 +1,19 @@
-
+const rapid=(inputString) => {
+    var newString = ""
+    let lowerString = inputString.toLowerCase()
+    for (let i = 0; i < lowerString.length; i++){ 
+        if (
+            lowerString[i] != "a" &&
+            lowerString[i] != "e" &&
+            lowerString[i] != "i" &&
+            lowerString[i] != "o" &&
+            lowerString[i] != "u"
+        ){
+        newString = newString + lowerString[i].toUpperCase()
+        }
+    }
+        return newString;
+}
    // From this line up Do not change code below
    let str = "John";
    console.log(rapid(str));
